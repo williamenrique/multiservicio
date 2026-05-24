21a647ee8e9aa770affd493f8e550727161714fb
william enrique <william21enrique@gmail.com>
2026-05-23 20:16:21 -0400

    logo en login

--- app/Views/auth/Login.php
+++ app/Views/auth/Login.php
@@ -6,7 +6,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <link rel="shortcut icon" href="<?php echo URL_IMG; ?>logo1.png" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo URL_IMG; ?>logo.png" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo s($data['titulo']); ?> | Taller Pro</title>
     <script src="https://cdn.tailwindcss.com"></script>
