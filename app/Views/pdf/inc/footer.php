b9f4b8a775b97c455c2ea30d458ba706271efce8
william enrique <william21enrique@gmail.com>
2026-05-26 08:59:44 -0400

    construcion

--- app/Views/pdf/inc/footer.php
+++
@@ -1,8 +0,0 @@
-<div style="position: fixed; bottom: -20px; left: 0; right: 0; height: 50px; text-align: center; border-top: 1px solid #eee; padding-top: 10px;">
-    <p style="font-size: 9px; color: #999;">
-        Documento oficial generado por Taller Pro - <?php echo date('d/m/Y - h:i A'); ?><br>
-        Página <script type="text/php">echo $PAGE_NUM;</script> de <script type="text/php">echo $PAGE_COUNT;</script>
-    </p>
-</div>
-</body>
-</html>
